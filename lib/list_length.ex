72c63a193f3d17fa8cfb584b762d77bc2852a554
Raphael Queiroz <uroboros.phael@gmail.com>
2021-07-20 20:56:01 -0300

    feat: calculate list length using pattern matching and recursion.

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,13 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    calculate_list_length(list, 0)
+  end
+
+  defp calculate_list_length([], acc) do
+    acc
+  end
+
+  defp calculate_list_length([_head | tail], acc) do
+    calculate_list_length(tail, acc + 1)
   end
 end
